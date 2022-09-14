c834d403a26694f0c9b5c60e2a84ec0c73e9cc36
yusaincedere <yusaincedere@gmail.com>
2022-09-14 11:55:25 +0300

    sad

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate} from "react-router-dom";
 import { Navbar } from './component/Navbar';
 import { HomePage } from './Pages/HomePage';
 import Login from './Pages/Logins';
+import AdminPage from './Pages/AdminPage';
 function App() {
   return (
     <div className=''>
@@ -10,6 +11,7 @@ function App() {
       <Routes>
       <Route exact path="/" element={<HomePage/>} />
       <Route path="/login" element={<Login/>} />
+      <Route exact path="/dashboard" element={<AdminPage/>} />
       <Route path="/" element={<Navigate replace to="/"/>}/>
     </Routes>
     </div>
