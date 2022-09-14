81c8be66e4c41d311a9f896855913aa8be8abd33
yusaincedere <yusaincedere@gmail.com>
2022-09-14 11:55:15 +0300

    update

--- src/Pages/Logins.js
+++ src/Pages/Logins.js
@@ -1,9 +1,17 @@
 import React, {  useState } from "react"
+import { useNavigate } from "react-router-dom";
 import Input from "../component/Input";
 import '../App.css';
 const Login = (props) => {
     const [username, setUsername] = useState()
     const [password, setPassword] = useState()
+
+    const navigate = useNavigate();
+    const handleLogin = () =>{
+         navigate('/dashboard')
+    }
+
+
 
     const disabled = username && password
     return (
@@ -12,7 +20,7 @@ const Login = (props) => {
             <Input name="username" label="Username" onChange={(change)=>{setUsername(change.target.value)}}></Input>
             <Input name="password" label="Password" type="password" onChange={(change)=>{setPassword(change.target.value)}} ></Input>
             <div className="text-center">
-                <button type="submit" className="button-background mt-4" disabled={!disabled}>Sign In</button>
+                <button onClick={handleLogin} type="submit" className="button-background mt-4" disabled={!disabled}>Sign In</button>
             </div>
             </form>
         </div>
